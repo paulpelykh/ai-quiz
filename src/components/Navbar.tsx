a06a9315c62a416929f3c35221ee3c20230391f3
Paul <eeddll1505@gmail.com>
2023-08-20 20:33:42 +0300

    added logo to Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,15 +1,22 @@
 import { getAuthSession } from '@/lib/nextauth'
+import Link from 'next/link'
 import React from 'react'
 
 type Props = {}
 
 const Navbar = async (props: Props) => {
-    const session = await getAuthSession()
-    if (session?.user) {
-        return <pre> {JSON.stringify(session.user, null, 2)}</pre>
-    } else {
-        return <div>Not signed in</div>
-    }
+    return (
+        <div className='fixed inset-0 top-0 bg-white dark:bg-gray-950 z-[10] h-fit bordre-b border-zinc-300 py-2'>
+            <div className='flex items-center justify-between h-full gap-2 px-8 mx-auto max-w-7xl'>
+                {/* Logo */}
+                <Link href='/' className='flex items-center gap-2'>
+                    <p className='rounded-lg border-2 border-b-4 border-r-4 border-black px-2 py-1 text-xl font-bold transition-all hover:-translate-y-[2px] md:block dark:border-white '>
+                        AI-Quiz
+                    </p>
+                </Link>
+            </div>
+        </div>
+    )
 }
 
 export default Navbar
