a3b0518232e54f41aa45f0f715f1be3ac2d591df
Paul <eeddll1505@gmail.com>
2023-08-23 17:00:56 +0300

    added Quiz Creation form

--- src/components/QuizCreation.tsx
+++ src/components/QuizCreation.tsx
@@ -1,3 +1,4 @@
+'use client';
 import React from 'react';
 import {
   Card,
@@ -6,10 +7,44 @@ import {
   CardHeader,
   CardTitle,
 } from './ui/card';
+import { useForm } from 'react-hook-form';
+import { quizCreationSchema } from '@/schemas/form/quiz';
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from './ui/form';
+import { Button } from './ui/button';
+import { Input } from './ui/input';
+import { BookOpen, CopyCheck } from 'lucide-react';
+import { Separator } from '@radix-ui/react-dropdown-menu';
 
 type Props = {};
 
+type Input = z.infer<typeof quizCreationSchema>;
+
 const QuizCreation = (props: Props) => {
+  const form = useForm<Input>({
+    resolver: zodResolver(quizCreationSchema),
+    defaultValues: {
+      amount: 3,
+      topic: '',
+      type: 'mcq',
+    },
+  });
+
+  function onSubmit(input: Input) {
+    alert(JSON.stringify(input, null, 2));
+  }
+
+  form.watch();
+
   return (
     <div className="absolute top-1/2 left-1/2 -translate-y-1/2">
       <Card>
@@ -17,7 +52,77 @@ const QuizCreation = (props: Props) => {
           <CardTitle className="text-2x font-bold">Quiz Creation</CardTitle>
           <CardDescription>Choose a topic</CardDescription>
         </CardHeader>
-        <CardContent></CardContent>
+        <CardContent>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+              <FormField
+                control={form.control}
+                name="topic"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Topic</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Enter a topic..." {...field} />
+                    </FormControl>
+                    <FormDescription>Please provide a topic</FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="amount"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Number of Questions</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="Enter an amount..."
+                        {...field}
+                        type="number"
+                        min={1}
+                        max={10}
+                        onChange={e => {
+                          form.setValue('amount', parseInt(e.target.value));
+                        }}
+                      />
+                    </FormControl>
+                    <FormDescription>Please provide a topic</FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <div className="flex justify-between">
+                <Button
+                  variant={
+                    form.getValues('type') === 'mcq' ? 'default' : 'secondary'
+                  }
+                  className="w-1/2 rounded-none rounded-l-lg"
+                  onClick={() => {
+                    form.setValue('type', 'mcq');
+                  }}
+                  type="button"
+                >
+                  <CopyCheck className="w-4 h-4 mr-2" /> Multiple Choice
+                </Button>
+                <Separator />
+                <Button
+                  variant={
+                    form.getValues('type') === 'open_ended'
+                      ? 'default'
+                      : 'secondary'
+                  }
+                  className="w-1/2 rounded-none rounded-r-lg"
+                  onClick={() => form.setValue('type', 'open_ended')}
+                  type="button"
+                >
+                  <BookOpen className="w-4 h-4 mr-2" /> Open Ended
+                </Button>
+              </div>
+              <Button type="submit">Submit</Button>
+            </form>
+          </Form>
+        </CardContent>
       </Card>
     </div>
   );
