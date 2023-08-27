12d95bffab5e63cb3c32d9b5b4d954c066ae7949
Paul <eeddll1505@gmail.com>
2023-08-27 18:55:03 +0300

    changed functionality of getting questions

--- src/app/api/questions/route.ts
+++ src/app/api/questions/route.ts
@@ -23,28 +23,20 @@ export const POST = async (req: Request, res: Response) => {
     let questions: any;
     if (type === 'open_ended') {
       questions = await strict_output(
-        'You are a helpful AI that is able to generate a pair of questions and answers, the lenght should not exeed 15 words, store all the pairs of answers and questions in a JSON array',
-        new Array(amount).fill(
-          `You are to generate a random hard open-ended question about ${topic}`
-        ),
-        {
-          question: 'question',
-          answer: 'answer with max length of 15 words',
-        }
+        'You are a helpful AI that is able to generate a pair of questions and answers, the lenght should not exeed 15 words, store all the pairs of answers and questions in a JSON array'
+        // {
+        //   question: 'question',
+        //   answer: 'answer with max length of 15 words',
+        // }
       );
     } else if (type === 'mcq') {
+      //TODO: make prompt dynamic
       questions = await strict_output(
-        'You are a helpful AI that is able to generate mcq questions and answers, the length of each answer should not exceed 15 words',
-        new Array(amount).fill(
-          `You are to generate a random mcq question about ${topic}`
-        ),
-        {
-          question: 'question',
-          answer: 'answer with max length of 15 words',
-          option1: '1st option with max length of 15 words',
-          option2: '2nd option with max length of 15 words',
-          option3: '3d option with max length of 15 words',
-        }
+        `I want to make a quiz about javascript.
+        I want 10 questions, with 4 answers per question.
+The questions should be in English.
+Please give me 1 correct answer for each question.
+The question can not be longer than 120 characters, and the answers can not be longer than 75 characters.`
       );
     }
     return NextResponse.json(
--- src/lib/gpt.ts
+++ src/lib/gpt.ts
@@ -5,22 +5,98 @@ const configuration = new Configuration({
 });
 const openai = new OpenAIApi(configuration);
 
+interface OutputFormat {
+  [key: string]: string | string[] | OutputFormat;
+}
+export interface Question {
+  question: string;
+  answers: string[];
+  correctAnswerPositions: number[];
+}
+
+export interface QueryQuestionsResponse {
+  questions: Question[];
+  requestMessage: string;
+  responseMessage: string;
+}
+
+export interface ResultType {
+  questions: Question[];
+  requestMessage: string;
+  responseMessage: string;
+}
+
 export async function strict_output(
-  system_prompt: any,
-  user_prompt: any,
-  output_format: any
+  // system_prompt: string,
+  user_prompt: string | string[],
+  output_format: string = `Format: Question|Answer1|Answer2|Answer3|Answer4
+A # mark indicates the correct answer.
+Each question and its answers should be on a single line.
+Before each question, please write the question number with a $ sign in front.
+Example response: 
+"$1. What is the capital of France?|Paris#|London|Berlin|Madrid
+$2. How many letters are in the english alphabet?|30|24|28|26#"`,
+  default_category: string = '',
+  output_value_only: boolean = false,
+  model: string = 'gpt-3.5-turbo',
+  temperature: number = 1,
+  num_tries: number = 3,
+  verbose: boolean = false
 ) {
+  let error_msg: string = '';
+
+  // Use OpenAI to get a response
   const response = await openai.createChatCompletion({
-    model: 'gpt-3.5-turbo',
+    temperature: temperature,
+    model: model,
     messages: [
-      { role: 'system', content: system_prompt },
+      {
+        role: 'system',
+        content: output_format + error_msg,
+      },
       { role: 'user', content: user_prompt.toString() },
     ],
   });
 
-  const res =
-    response.data.choices[0].message?.content?.replace(/'/g, '"') || '';
+  const message = `message`;
 
-  const output = JSON.parse(res);
-  return output;
+  let res: string =
+    response.data.choices[0].message?.content?.replace(/'/g, '"') ?? '';
+
+  const questions: Question[] = [];
+  for (const line of res.split('\n')) {
+    if (line.startsWith('$') || line.match(/^\d/)) {
+      // If line starts with a $ or a number
+      const line_split = line.split('|');
+      let [first, ...rest] = line_split[0].split('. ');
+      const question = rest.join('. ').trim(); // Remove the question number
+      const answers = line_split.slice(1).map(x => x.trim());
+      const correctAnswerPositions = answers
+        .map((a, i) => (a.includes('#') ? i : -1))
+        .filter(i => i !== -1);
+
+      // Shuffle the answers around and adjust the correct answer positions accordingly
+      for (let i = answers.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [answers[i], answers[j]] = [answers[j], answers[i]];
+        correctAnswerPositions.forEach((p, k) => {
+          if (p === i) correctAnswerPositions[k] = j;
+          else if (p === j) correctAnswerPositions[k] = i;
+        });
+      }
+
+      questions.push({
+        question,
+        answers: answers.map(a => a.replace('#', '')),
+        correctAnswerPositions,
+      });
+    }
+  }
+  const r: ResultType = {
+    questions: questions,
+    requestMessage: message,
+    responseMessage: res,
+  };
+
+  return questions;
 }
