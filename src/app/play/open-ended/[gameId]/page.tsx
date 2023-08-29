6f0f4c4f88b46e354906707c81dd48eb195aebc6
Paul <eeddll1505@gmail.com>
2023-08-29 16:36:16 +0300

    make styling for MCQ page

---
+++ src/app/play/open-ended/[gameId]/page.tsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+type Props = {};
+
+type State = {};
+
+class OpenEndedPage extends Component<Props, State> {
+  state = {};
+
+  render() {
+    return <div>OpenEndedPage</div>;
+  }
+}
+
+export default OpenEndedPage;
