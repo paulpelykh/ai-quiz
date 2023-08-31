008cead7dfa682058473003edaa38a3e64e14483
Paul <eeddll1505@gmail.com>
2023-08-31 17:41:22 +0300

    deleted config

--- src/app/api/questions/route.ts
+++ src/app/api/questions/route.ts
@@ -4,9 +4,6 @@ import { ZodError } from 'zod';
 import { strict_output } from '@/lib/gpt';
 import { getAuthSession } from '@/lib/nextauth';
 
-export const runtime = 'edge'; // 'nodejs' is the default
-export const preferredRegion = 'iad1'; // only execute this function on iad1
-
 // api/questions
 export const POST = async (req: Request, res: Response) => {
   try {
