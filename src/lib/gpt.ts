7a7870432b7f15e213f70d16acf8b99edb643d0c
Paul <eeddll1505@gmail.com>
2023-09-01 18:14:15 +0300

    changes in config

--- src/lib/gpt.ts
+++ src/lib/gpt.ts
@@ -1,5 +1,8 @@
 import { Configuration, OpenAIApi } from 'openai';
 
+export const config = {
+  runtime: 'edge',
+};
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
