cf371e859f534a467c085ee2d143f565f5c6959c
Paul <eeddll1505@gmail.com>
2023-09-02 16:57:19 +0300

    config changes

--- src/app/api/questions/route.ts
+++ src/app/api/questions/route.ts
@@ -4,6 +4,10 @@ import { ZodError } from 'zod';
 import { strict_output } from '@/lib/gpt';
 import { getAuthSession } from '@/lib/nextauth';
 
+export const config = {
+  runtime: 'edge',
+};
+
 // api/questions
 export const POST = async (req: Request, res: Response) => {
   try {
